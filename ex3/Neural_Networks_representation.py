6eefcc7afbce4aa6c99aef45f519c1d3fc03fcea
Emanuel Pintilii <emanuel.pintilii@arcadis.com>
2019-02-22 17:23:49 +0200

    "Stoped at backpropagation"

--- ex3/Neural_Networks_representation.py
+++ ex3/Neural_Networks_representation.py
@@ -1,10 +1,12 @@
+### Week 3 Neural Networks Representation
+
 import os
 import scipy.io
 import scipy.special
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-import itertools
+import math
 
 CWD = os.getcwd()
 folder_weights = "data\ex3weights.mat"
@@ -56,15 +58,16 @@ print("Training set accuracy: %s" %(100*(n_correct/n_total)))
 
 
 
-for i in range(5):
-    image_index = random.choice(incorect_indices)
-    image_matrix = X[image_index, 1:].reshape([20, 20]).T
-    plt.imshow(image_matrix, cmap="gray")
-    predicted_val = predictNN(X[image_index], myThetas)
-    predicted_val = 0 if predicted_val == 10 else predicted_val
-    plt.title("Predicted: %d" % predicted_val)
-    plt.show()
+# for i in range(5):
+#     image_index = random.choice(incorect_indices)
+#     image_matrix = X[image_index, 1:].reshape([20, 20]).T
+#     plt.imshow(image_matrix, cmap="gray")
+#     predicted_val = predictNN(X[image_index], myThetas)
+#     predicted_val = 0 if predicted_val == 10 else predicted_val
+#     plt.title("Predicted: %d" % predicted_val)
+#     plt.show()
 
+###From here starts the Week4 - Neural Networks Learning
 
 input_layer_size = 400
 hidden_layer_size = 25
@@ -72,12 +75,80 @@ output_layer_size = 10
 
 n_training_samples = X.shape[0]
 
-def flattenParams(thetas_list):
-    flattened_list = [mytheta.flatten() for mytheta in thetas_list]
-    combined = list(itertools.chain.from_iterable(flattened_list))
-    assert len(combined) == (input_layer_size+1)*hidden_layer_size + (hidden_layer_size +1) * output_layer_size
-    return np.array(combined).reshape((len(combined),1))
+def flattenParams(myThetas):
+    theta_flatten = []
+    for theta in myThetas:
+        th_fla = theta.flatten()
+        theta_flatten.extend(th_fla)
+    assert len(theta_flatten) == (input_layer_size+1)*hidden_layer_size +(hidden_layer_size +1) * output_layer_size
+    return np.array(theta_flatten).reshape((len(theta_flatten),1))
 
-a = Theta1.flatten()
-b = list(itertools.chain.from_iterable(a))
-print(b)
+
+def reshapeParams(flattened_array):
+    theta1 = flattened_array[:(input_layer_size+1)*hidden_layer_size].reshape((hidden_layer_size, input_layer_size+1))
+    theta2 = flattened_array[(input_layer_size+1)*hidden_layer_size:].reshape((output_layer_size, hidden_layer_size+1))
+    return [theta1, theta2]
+
+def flattenX(myX):
+    return np.array(myX.flatten()).reshape((n_training_samples*(input_layer_size+1),1))
+
+def reshapeX(flattenedX):
+    return np.array(flattenedX).reshape((n_training_samples, input_layer_size+1))
+
+def computeCost(mythetas_flattened, myX_flattened, myy, mylambda = 0.0):
+    mythetas = reshapeParams(mythetas_flattened)
+    myX = reshapeX(myX_flattened)
+    total_cost = 0.0
+    m = n_training_samples
+    for irow in range(m):
+        myrow = myX[irow]
+        myhs = propagateForward(myrow, mythetas)[-1][1]
+        tmpy = np.zeros((10,1))
+        tmpy[myy[irow]-1] = 1
+        cost_myrow = -tmpy.T.dot(np.log(myhs))-(1-tmpy.T).dot(np.log(1-myhs))
+        total_cost += cost_myrow
+    total_cost = float(total_cost)/m
+    total_reg = 0.0
+    for mytheta in myThetas:
+        total_reg += np.sum(mytheta*mytheta)
+    total_reg *= float(mylambda)/(2*m)
+    return total_cost + total_reg
+
+
+def propagateForward(row, Thetas):
+    features = row
+    zs_as_per_layer = []
+    for i in range(len(Thetas)):
+        Theta = Thetas[i]
+        z = Theta.dot(features).reshape((Theta.shape[0],1))
+        a = exponential(z)
+        zs_as_per_layer.append((z, a))
+        if i == len(Thetas)-1:
+            return np.array(zs_as_per_layer)
+        a = np.insert(a, 0, 1)
+        features = a
+
+
+def exponential(z):
+    a = np.zeros(z.shape)
+    for i in range(len(z)):
+        sigmoid = 1/(1+math.exp(-z[i]))
+        a[i]=sigmoid
+    return a
+
+
+def exponential2(z):
+    a = []
+    for i in range(len(z)):
+        sigmoid = 1/(1+math.exp(-z[i]))
+        a.append(sigmoid)
+    a = np.array(a).reshape(z.shape)
+    return a
+
+print(computeCost(flattenParams(myThetas), flattenX(X), y))
+
+###Backpropagation
+
+def sigmoidGradient(z):
+    activation = exponential(z)
+    return activation*(1-activation)
